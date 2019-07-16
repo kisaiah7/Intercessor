be14b623a49dd4ceca4d7520d377c7334500f26f
yanghakim <yanghakim0@gmail.com>
2019-07-16 10:24:40 -0400

    changed res.render to res.send

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -82,7 +82,7 @@ module.exports = app => {
   }
 
   app.get("/menu", requireLogin, function(req, res) {
-    res.render("Menu", { user: req.user });
+    res.send("Menu", { user: req.user });
   });
 
   /*
--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -40,7 +40,7 @@ module.exports = app => {
       console.log("Message sent: %s", info.messageId);
       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-      res.render("contact", { msg: "Email has been sent" });
+      res.send("contact", { msg: "Email has been sent" });
     });
   });
 };
